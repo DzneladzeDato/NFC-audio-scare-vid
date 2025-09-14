565e1c0459667dfdcf98cad5d92c36f5625aeb76
DzneladzeDato <155750299+DzneladzeDato@users.noreply.github.com>
2025-09-14 20:33:12 +0400

    Update script.js

--- script.js
+++ script.js
@@ -2,19 +2,20 @@ const playBtn = document.getElementById("playBtn");
 const video = document.getElementById("myVideo");
 
 playBtn.addEventListener("click", () => {
-  video.style.display = "block"; // Show video
+  video.style.display = "block"; 
   video.play();
-  video.volume = 1.0; // Full volume
+  video.volume = 1.0;
 
-  // Hide button when video starts
+  // Hide everything else
   playBtn.style.display = "none";
+  document.body.style.background = "black";
 
   // Request fullscreen
   if (video.requestFullscreen) {
     video.requestFullscreen();
-  } else if (video.webkitRequestFullscreen) { // Safari
+  } else if (video.webkitRequestFullscreen) {
     video.webkitRequestFullscreen();
-  } else if (video.msRequestFullscreen) { // IE/Edge
+  } else if (video.msRequestFullscreen) {
     video.msRequestFullscreen();
   }
 });
